f425b8cb3644cb295717952786dcaeef09f13d49
irma1991 <irmalinartaite@gmail.com>
2020-01-26 16:01:56 +0200

    neveikia update

--- templates/main/pages/delete_film.page.php
+++ templates/main/pages/delete_film.page.php
@@ -1,26 +1,74 @@
 <h2>Filmo istrynimas</h2>
 <?php
-$dsn = "mysql:host=$host;dbname=$db";
+
+$dns= "mysql:host=$host;dbname=$db";
 try{
-    $conn = new PDO($dsn, $username, $password, $options);
+    $conn = new PDO($dns, $username, $password, $options);
     if($conn){
-        if(isset($GET_['submit'])){
-            $kuriTrinam = $_GET['id'];
-            $stmt = $conn->query ("DELETE FROM lentele_filmai
-                                            WHERE id =$kuriTrinam");
-        }
+        $kuriTrinam = $_GET['id'];
+        $stmt = $conn->query("SELECT lentele_filmai.id, lentele_filmai.pavadinimas, lentele_filmai.aprasymas, 
+                              lentele_filmai.metai, lentele_filmai.rezisierius, lentele_filmai.imdb, lentele_zanrai.pavadinimas AS zanroPavadinimas
+                              FROM lentele_filmai 
+                              INNER JOIN lentele_zanrai ON lentele_filmai.genre_id=lentele_zanrai.id
+                              WHERE lentele_filmai.id = $kuriTrinam");
+        $filmas = $stmt->fetch();
     }
-} catch (PDOException $e){
+} catch (PDOException $e) {
     echo $e->getMessage();
+} ?>
+<?php if (isset($_POST['submit'])){
+    try {
+        if ($conn){
+            $sql = "DELETE FROM lentele_filmai                
+                    WHERE id = :id";
+            $stmt= $conn->prepare($sql);
+            $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_STR);
+            $stmt->execute();
+            header('Location:'.path.'?page=filmu-valdymas');
+        }
+    } catch (PDOException $e){
+        echo $e->getMessage();
+    }
 }
 
 ?>
-<form method="get">
-    <div class="container alert alert-danger" role="alert">
-        Ar tikrai norite istrinti filma?
-    </div>
+
 <div class="container">
-    <button type="submit" name = "submit" class="btn btn-secondary">Patvirtinti</button>
+    <form method="post">
+        <div class="alert alert-danger" role="alert">Ar tikrai norite istrinti filma?</div>
+        <div class="form-group">
+            <label for="id">Filmo ID</label>
+            <input type="text" class="form-control" id="id" name="id"  value="<?=$filmas['id']; ?>">
+        </div>
+        <div class="form-group">
+            <label for="pavadinimas">Filmo pavadinimas</label>
+            <input type="text" class="form-control" id="pavadinimas" name="pavadinimas"  value="<?=$filmas['pavadinimas']; ?>">
+        </div>
+        <div class="form-group">
+            <label for="aprasymas">Aprasymas</label>
+            <input type="text" class="form-control" id="aprasymas" name="aprasymas" value="<?=$filmas['aprasymas']; ?>">
+        </div>
+        <div class="form-group">
+            <label for="metai">Metai</label>
+            <input type="text" class="form-control" id="metai" name="metai"  value="<?=$filmas['metai']; ?>">
+        </div>
+        <div class="form-group">
+            <label for="rezisierius">Rezisierius</label>
+            <input type="text" class="form-control" id="rezisierius" name="rezisierius" value="<?=$filmas['rezisierius']; ?>">
+        </div>
+        <div class="form-group">
+            <label for="ivertinimai">Ivertinimas</label>
+            <input type="text" class="form-control" id="imdb" name="imdb" value="<?=$filmas['imdb']; ?>">
+        </div>
+
+        <div class="form-group">
+            <label for="zanras">Filmo zanras</label>
+            <input type="text" class="form-control" id="zanras" name="zanras"  value="<?=$filmas['zanroPavadinimas']; ?>">
+        </div>
+        <div class="form-group">
+            <a href="?page=filmu-valdymas" class="btn btn-success">Atsaukti</a>
+            <button type="submit" name="submit" class="btn btn-danger">Istrinti</button>
+        </div>
+    </form>
 </div>
-</form>
 
--- templates/main/pages/update_film.page.php
+++ templates/main/pages/update_film.page.php
@@ -1,31 +1,44 @@
+<h1>Filmo redagavimas</h1>
 <?php
-$dsn = "mysql:host=$host;dbname=$db";
-$kuriRedaguojam = $_GET['id'];
+
+$dns= "mysql:host=$host;dbname=$db";
 try{
-    $conn = new PDO($dsn, $username, $password, $options);
+    $conn = new PDO($dns, $username, $password, $options);
     if($conn){
-        $stmt = $conn->query ("SELECT * FROM lentele_filmai
-                                       WHERE id=$kuriRedaguojam");
+        $kuriUpdatinam = $_GET['id'];
+        $stmt = $conn->query("SELECT lentele_filmai.id, lentele_filmai.pavadinimas, lentele_filmai.aprasymas, 
+                              lentele_filmai.metai, lentele_filmai.rezisierius, lentele_filmai.imdb, lentele_zanrai.pavadinimas AS zanroPavadinimas
+                              FROM lentele_filmai 
+                              INNER JOIN lentele_zanrai ON lentele_filmai.genre_id=lentele_zanrai.id 
+                              WHERE lentele_filmai.id = $kuriUpdatinam");
         $filmas = $stmt->fetch();
     }
-} catch (PDOException $e){
+} catch (PDOException $e) {
     echo $e->getMessage();
-}?>
+} ?>
 
 <?php if (isset($_POST['submit'])){
     try {
         if ($conn){
-            $sql = "UPDATE lentele_filmai SET (pavadinimas, aprasymas, metai, rezisierius, imdb, genre_id),
-                    VALUES (:pavadinimas, :aprasymas, :metai, :rezisierius, :imdb, :genre_id)";
+            $sql = "UPDATE lentele_filmai SET pavadinimas = :pavadinimas, 
+                                          aprasymas = :aprasymas, 
+                                          metai = :metai, 
+                                          rezisierius =:rezisierius, 
+                                          imdb = :imdb, 
+                                          genre_id = :genre_id
+                                          WHERE id = :id";
             $stmt= $conn->prepare($sql);
+            $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_STR);
             $stmt->bindParam(':pavadinimas', $_POST['pavadinimas'], PDO::PARAM_STR);
             $stmt->bindParam(':aprasymas', $_POST['aprasymas'], PDO::PARAM_STR);
             $stmt->bindParam(':metai', $_POST['metai'], PDO::PARAM_STR);
             $stmt->bindParam(':rezisierius', $_POST['rezisierius'], PDO::PARAM_STR);
-            $stmt->bindParam(':imdb', $_POST['imdb'], PDO::PARAM_STR);
-            $stmt->bindParam(':genre_id', $_POST['zanras'], PDO::PARAM_STR);
+            $stmt->bindParam(':imdb', $_POST['ivertinimai'], PDO::PARAM_STR);
+            $stmt->bindParam(':genre_id', $_POST['zanroPavadinimas'], PDO::PARAM_STR);
             $stmt->execute();
             header('Location:'.path.'?page=filmu-valdymas');
+
+
         }
     } catch (PDOException $e){
         echo $e->getMessage();
@@ -34,32 +47,37 @@ try{
 
 ?>
 
-<div class = "container">
+<div class="container">
     <form method="post">
         <div class="form-group">
-            <label>Filmo pavadinimas</label>
-            <input class="form-control" id="pavadinimas" name="pavadinimas" value="<?=$filmas['pavadinimas'];?>">
+            <label for="id">Filmo id</label>
+            <input type="text" class="form-control" id="id" name="id"  value="<?=$filmas['id']; ?>">
         </div>
         <div class="form-group">
-            <label>Aprasymas</label>
-            <input class="form-control" id="aprasymas" name="aprasymas" value="<?=$filmas['aprasymas'];?>">
+            <label for="pavadinimas">Filmo pavadinimas</label>
+            <input type="text" class="form-control" id="pavadinimas" name="pavadinimas"  value="<?=$filmas['pavadinimas']; ?>">
         </div>
         <div class="form-group">
-            <label>Metai</label>
-            <input class="form-control" id="metai" name="metai" value="<?=$filmas['metai'];?>">
+            <label for="aprasymas">Aprasymas</label>
+            <input type="text" class="form-control" id="aprasymas" name="aprasymas" value="<?=$filmas['aprasymas']; ?>">
         </div>
         <div class="form-group">
-            <label>Rezisierius</label>
-            <input class="form-control" id="rezisierius" name="rezisierius" value="<?=$filmas['rezisierius'];?>">
+            <label for="metai">Filmo metai</label>
+            <input type="text" class="form-control" id="metai" name="metai"  value="<?=$filmas['metai']; ?>">
         </div>
         <div class="form-group">
-            <label>IMDB</label>
-            <input class="form-control" id="imdb" name="imdb" value="<?=$filmas['imdb'];?>">
+            <label for="rezisierius">Rezisierius</label>
+            <input type="text" class="form-control" id="rezisierius" name="rezisierius" value="<?=$filmas['rezisierius']; ?>">
         </div>
         <div class="form-group">
-            <label>Zanras</label>
-            <input class="form-control" id="zanras" name="zanras" value="<?=$filmas['genre_id'];?>">
+            <label for="ivertinimai">Ivertinimai</label>
+            <input type="text" class="form-control" id="ivertinimai" name="ivertinimai" value="<?=$filmas['imdb']; ?>">
         </div>
-        <button type="submit" class="btn btn-primary" id="submit" name="submit">Submit</button>
+
+        <div class="form-group">
+            <label for="zanras">Filmo zanras</label>
+            <input type="text" class="form-control" id="zanras" name="zanras"  value="<?=$filmas['zanroPavadinimas']; ?>">
+        </div>
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
